801016ba386e5a22e25b1fb5a4cfc386a2cb9533
Sharan Prasannaa <sharanprasasnnaar@gmail.com>
2026-02-14 23:33:45 +0530

    Bookings relationship added in eventype

--- backend/app/Models/EventType.php
+++ backend/app/Models/EventType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\User;
+use App\Models\Bookings;
 
 class EventType extends Model
 {
@@ -21,5 +22,10 @@ class EventType extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(\App\Models\Booking::class);
     }
 }
